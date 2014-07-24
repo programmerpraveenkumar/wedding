06782139d46c55d608aa3863596151716d795e90
praveen kumar <programmerpraveenkumar@gmail.com>
2014-07-24 18:30:47 +0530

    new design

---
+++ include/header.php
@@ -0,0 +1,72 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+<!--<title>Rockfort Video vision</title>-->
+<title></title>
+
+<!--Main Element CSS-->
+<link href="<?php echo INCLUDE_FILE ?>css/style.css" rel="stylesheet" type="text/css">
+<link href="<?php echo INCLUDE_FILE ?>css/hexagon.css" rel="stylesheet" type="text/css">
+<link href="<?php echo INCLUDE_FILE ?>css/form.css" rel="stylesheet" type="text/css">
+
+<!--Flexy Menu CSS-->
+<link href="<?php echo INCLUDE_FILE ?>css/flexy-menu.css" rel="stylesheet" type="text/css">
+
+<!--Responsive CSS-->
+<link href="<?php echo INCLUDE_FILE ?>css/responsive.css" rel="stylesheet" type="text/css">
+
+<!--Multitransition Gallery Sliders CSS-->
+<link href="<?php echo INCLUDE_FILE ?>css/audioPlayer_tr.css" rel="stylesheet" type="text/css">
+<link href="<?php echo INCLUDE_FILE ?>css/flashblock.css" rel="stylesheet" type="text/css">
+<link href="<?php echo INCLUDE_FILE ?>css/playlistBottomInside.css" rel="stylesheet" type="text/css">
+<link href="<?php echo INCLUDE_FILE ?>css/videoPlayer.css" rel="stylesheet" type="text/css">
+
+<!--Google Fonts-->
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900|Oswald:400,300,700' rel='stylesheet' type='text/css'>
+
+<!--[if lte IE 8]>
+<meta HTTP-EQUIV="REFRESH" content="0; url=lte-ie8.html">
+<![endif]-->
+
+</head>
+
+<body>
+
+	<!--Header Start-->
+    <header class="header-section">
+    	<div class="wrapper header">
+        	
+            <div class="logo">
+            	<a href="index.html"><img src="images/logo.png" alt="logo" /></a>
+            </div>
+            
+            <!--Navigation Menu Start-->
+            <ul class="flexy-menu thick">
+            	<li class="active"><a href="index.html">Home</a><div class="arrow-down"></div>
+                	
+            	</li>
+                <li><a href="about-us.html">About Us</a><div class="arrow-down"></div>
+                	
+                               	
+                </li>
+                <li><a href="gallery-fullwidth.html">Galleries</a><div class="arrow-down"></div>
+                
+                </li>
+                <li><a href="view.html">360</a><div class="arrow-down"></div>	
+                </li>
+                
+                <li><a href="gallery-fullscreen.html">Videos</a><div class="arrow-down"></div></li>
+                </li>
+                
+                <li><a href="clients.html">Clients</a><div class="arrow-down"></div>
+                	
+                </li>
+                <li><a href="contact.html">Contact</a><div class="arrow-down"></div></li>
+            </ul>
+            <!--Navigation Menu End-->
+            
+        </div>
+    </header>
+    <!--Header End-->
